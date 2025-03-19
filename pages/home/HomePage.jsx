156a0443816be506832112105064dcb961e9e6cf
sandyddeveloper <sandyddeveloper@gmail.com>
2025-03-19 14:57:50 +0530

    repaired the product detailed tailwindcss config

--- pages/home/HomePage.jsx
+++ pages/home/HomePage.jsx
@@ -2,7 +2,6 @@ import MainLayout from '@/components/MainLayout'
 import React from 'react'
 import Hero from './container/Hero'
 import ProductCard from '@/components/ProductCard'
-import ProductDetail from '@/components/productDetails'
 
 const HomePage = () => {
   return (
